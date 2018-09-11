0e687d4b547117ba6fddf00bb8025b62f3b92872
Jose <cleyra.uzcategui@gmail.com>
2018-09-11 10:20:19 +0000

    Tested is the file doesn't exists

--- TimezoneTest/ReaderTest.cs
+++ TimezoneTest/ReaderTest.cs
@@ -12,12 +12,21 @@ namespace TimezoneTest
         public const string PATH_TO_FILE = @"..\..\";
 
         [TestMethod]
-        public void FileExists()
+        public void FileDoesExists()
         {
             string fileName = "Timezone.txt";
             string path = Path.Combine(Environment.CurrentDirectory, PATH_TO_FILE, fileName);
             Reader reader = new Reader();
             Assert.IsTrue(reader.fileExists(path));
+        }
+
+        [TestMethod]
+        public void FileDoesNotExists()
+        {
+            string fileName = "Badname.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, PATH_TO_FILE, fileName);
+            Reader reader = new Reader();
+            Assert.IsFalse(reader.fileExists(path));
         }
 
     }
